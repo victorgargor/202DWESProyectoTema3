877f2c1726594141277cffd8c405aeed6a5cfefc
victorgargor <victor.gargor@educa.jcyl.es>
2024-09-26 13:07:27 +0200

    Actualización 26/09/2024

---
+++ indexProyectoTema3.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="webroot/css/index.css" type="text/css">
+        <title>Víctor García Gordón</title>
+    </head>
+    <body>
+        <header>      
+            <h1 id="inicio">TEMA 3 CARACTERÍSTICAS DEL LENGUAJE PHP</h1>
+        </header>
+        <main>
+            <section>
+                <table>
+                    <tr>
+                        <th>nº</th>
+                        <th>Enunciado</th>
+                        <th>Ejecutar</th>
+                        <th>Mostrar</th>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>Hola mundo y phpinfo()</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php">Mostrar</a></td>
+                    </tr>
+                </table>
+            </section>       
+        </main>
+        <footer>
+            <div>
+                <a href="/index.html">Víctor García Gordón</a>
+                <a href="/202DWESProyectoDWES/indexProyectoDWES.php">DWES</a>
+                <a target="blank" href="doc/curriculum.pdf"><img src="doc/curriculum.jpg" alt="curriculum"></a>
+                <a target="blank" href="https://github.com/victorgargor"><img src="doc/github.png" alt="github"></a>
+            </div>
+        </footer>
+    </body>
+</html>
