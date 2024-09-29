500f93a6f6f4a6e3f7b1d0e2350d6543c60804e5
victorgargor <victor.gargor@educa.jcyl.es>
2024-09-29 20:13:08 +0200

    Actualización Css Index y enunciados de la tabla de ejercicios todo 29/09/2024

--- indexProyectoTema3.php
+++ indexProyectoTema3.php
@@ -21,9 +21,182 @@
                     </tr>
                     <tr>
                         <td>0</td>
-                        <td>Hola mundo y phpinfo()</td>
+                        <td>Hola mundo y phpinfo().</td>
                         <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
-                        <td><a href="mostrarcodigo/muestraEjercicio00.php">Mostrar</a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Inicializar variables de los distintos tipos de datos básicos y mostrar los datos por pantalla.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Inicializar y mostrar una variable heredoc.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Mostrar en tu página index la fecha y hora actual formateada en castellano.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Mostrar en tu página index la fecha y hora actual en Oporto formateada en portugués.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Inicializar y mostrar una variable que tiene una marca de tiempo.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Operar con fechas: calcular la fecha y el día de la semana de dentro de 60 días.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Mostrar el nombre del fichero que se está ejecutando.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>Mostrar la dirección IP del equipo desde el que estás accediendo.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>Mostrar el path donde se encuentra el fichero que se está ejecutando.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Mostrar el contenido del fichero que se está ejecutando.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>11</td>
+                        <td>Mostrar el documento PHPDoc del proyecto que se está ejecutando generado con PHP Documentor o ApiGen.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>12</td>
+                        <td>Mostrar el contenido de las variables superglobales (utilizando print_r() y foreach()).</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>13</td>
+                        <td>Crear una función que cuente el número de visitas a la página actual desde una fecha concreta.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>14</td>
+                        <td>Comprobar las librerías que estás utilizando en tu entorno de desarrollo y explotación. Crear tu propia librería de funciones y estudiar la
+                            forma de usarla en el entorno de desarrollo y en el de explotación.
+                        </td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>15</td>
+                        <td>Crear e inicializar un array con el sueldo percibido de lunes a domingo. Recorrer el array para calcular el sueldo percibido durante la
+                            semana.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>16</td>
+                        <td>Recorrer el array anterior utilizando funciones para obtener el mismo resultado.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>17</td>
+                        <td>Inicializar un array (bidimensional con dos índices numéricos) donde almacenamos el nombre de las personas que tienen reservado el
+                            asiento en un teatro de 20 filas y 15 asientos por fila. (Inicializamos el array ocupando únicamente 5 asientos). Recorrer el array con
+                            distintas técnicas (foreach(), while(), for()) para mostrar los asientos ocupados en cada fila y las personas que lo ocupan.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>18</td>
+                        <td>Recorrer el array anterior utilizando funciones para obtener el mismo resultado.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>19</td>
+                        <td>Construir una librería de funciones de validación de campos de formularios (LibreríaValidacionFormularios.php) para utilizarla en los
+                            siguientes ejercicios. Discusión: diferencia entre librería de funciones y clase.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>20</td>
+                        <td>Convertir la LibreriaValidacionFormularios.php en una clase ValidacionFormularios.php. El profesor facilitará a los alumnos la clase
+                            AAMMDDValidacionFormularios.php desarrollada en el curso anterior como punto de partida.
+                        </td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>21</td>
+                        <td>Construir un formulario para recoger un cuestionario realizado a una persona y enviarlo a una página Tratamiento.php para que muestre
+                            las preguntas y las respuestas recogidas.
+                        </td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>22</td>
+                        <td>Construir un formulario para recoger un cuestionario realizado a una persona y mostrar en la misma página las preguntas y las respuestas
+                            recogidas.
+                        </td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>23</td>
+                        <td>Construir un formulario para recoger un cuestionario realizado a una persona y mostrar en la misma página las preguntas y las respuestas
+                            recogidas; en el caso de que alguna respuesta esté vacía o errónea volverá a salir el formulario con el mensaje correspondiente.
+                        </td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>24</td>
+                        <td>Construir un formulario para recoger un cuestionario realizado a una persona y mostrar en la misma página las preguntas y las respuestas
+                            recogidas; en el caso de que alguna respuesta esté vacía o errónea volverá a salir el formulario con el mensaje correspondiente, pero las
+                            respuestas que habíamos tecleado correctamente aparecerán en el formulario y no tendremos que volver a teclearlas.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>25</td>
+                        <td>Trabajar en PlantillaFormulario.php mi plantilla para hacer formularios como churros.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
+                    </tr>
+                    <tr>
+                        <td>26</td>
+                        <td>Probar la plantilla anterior desarrollando un formulario que recoja la temperatura y la presión atmosférica en una serie de fechas y (cuando
+                            el usuario lo decida) genere un informe con los datos recibidos y un promedios, mínimos y máximos de temperatura y presión atmosférica.</td>
+                        <td><a href="codigoPHP/ejercicio00.php"><img src="doc/play.png" alt="play"></a></td>
+                        <td><a href="mostrarcodigo/muestraEjercicio00.php"><img src="doc/show.png" alt="show"></a></td>
                     </tr>
                 </table>
             </nav>       
