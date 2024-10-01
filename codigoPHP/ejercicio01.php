c76f8e455996677c2db95e86c597eb81a5cf1dfa
victorgargor <victor.gargor@educa.jcyl.es>
2024-10-01 13:57:11 +0200

    Clase 01/10/2024

--- codigoPHP/ejercicio01.php
+++ codigoPHP/ejercicio01.php
@@ -19,14 +19,32 @@
                 $entero = 8;
                 $decimal = 8.8;
                 $boolean = true;
-                $formato = "La variable \$decimal es de tipo ".gettype($decimal)." y tiene el valor %g"."\n";
-                $devolverBoolean = "La variable \$boolean es de tipo ".gettype($boolean)." y tiene el valor $boolean"."\n";
-                echo "La variable \$cadena es de tipo ".gettype($cadena)." y tiene el valor $cadena."."\n";
-                print "La variable \$entero es de tipo ".gettype($entero)." y tiene el valor $entero."."\n";
-                echo printf($formato, $decimal)."\n";
-                print_r($devolverBoolean)."\n";
-                var_dump($entero, $boolean)."\n";
-                ?>   
+                echo '<h2>Utilizando echo</h2> <br>';
+                echo'La variable <b>$cadena</b> es de tipo <b>'.gettype($cadena).'</b> y tiene el valor <b>'.$cadena.'</b><br>';
+                echo 'La variable <b>$entero</b> es de tipo <b>'.gettype($entero).'</b> y tiene el valor <b>'.$entero.'</b><br>';
+                echo 'La variable <b>$decimal</b> es de tipo <b>'.gettype($decimal).'</b> y tiene el valor <b>'.$decimal.'</b><br>';
+                echo 'La variable <b>$boolean</b> es de tipo <b>'.gettype($boolean).'</b> y tiene el valor <b>'.$boolean.'</b><br>';
+                $cadena = "Marcos"; 
+                $entero = 9; 
+                $decimal = 9.9; 
+                $boolean = false;
+                echo '<br> <h2>Utilizando print</h2> <br>';
+                print'La variable <b>$cadena</b> es de tipo '.gettype($cadena).'</b> y tiene el valor <b>'.$cadena.'</b><br>';
+                print 'La variable <b>$entero</b> es de tipo '.gettype($entero).'</b> y tiene el valor <b>'.$entero.'</b><br>';
+                print 'La variable <b>$decimal</b> es de tipo '.gettype($decimal).'</b> y tiene el valor <b>'.$decimal.'</b><br>';
+                print 'La variable <b>$boolean</b> es de tipo '.gettype($boolean).'</b> y tiene el valor <b>'.'"'.$boolean.'"'.'</b><br>';
+                $formato = 'La variable <b>$decimal</b> es de tipo <b>'.gettype($decimal).'</b> y tiene el valor <b>%g</b>';
+                $devolverBoolean = 'La variable <b>$boolean</b> es de tipo <b>'.gettype($boolean).'</b> y tiene el valor <b>'.'"'.$boolean.'"'.'</b><br>';
+                echo '<br> <h2>Utilizando printf</h2> <br>';              
+                echo printf($formato, $decimal).'<br>';             
+                echo '<br> <h2>Utilizando print_r</h2> <br>';
+                print_r($devolverBoolean);
+                echo '<br> <h2>Utilizando var_dump</h2> <br>';
+                var_dump($cadena);
+                var_dump($entero);
+                var_dump($decimal);
+                var_dump($boolean);
+                ?>
             </section>
         </main>
         <footer>
