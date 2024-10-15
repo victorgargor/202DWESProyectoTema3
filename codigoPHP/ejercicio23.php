41171b6e4bffe04d184e2a046a30c463aac1db8e
victorgargor <victor.gargor@educa.jcyl.es>
2024-10-15 08:59:56 +0200

    Trabajo Casa 15/10/2024

--- codigoPHP/ejercicio23.php
+++ codigoPHP/ejercicio23.php
@@ -14,48 +14,73 @@
         </header>
         <main>
             <?php
-            /**
-             * @author Víctor García Gordón
-             * @version Fecha de última modificación 09/10/2024
-             */
-            //Incluimos la libreria de validación de formularios
-            require_once '../core/231018libreriaValidacion.php';
-            //Variable que nos indica que todo va bien
-            $entradaOK = true;
-            //Array donde recogemos los mensajes de error
-            $aErrores = [
-                'nombre' => '',
-                'edad' => ''
-            ];
-            //Array donde recogeremos la respuestas correctas (si $entradaOK)
-            $aRespuestas = [
-                'nombre' => '',
-                'edad' => ''
-            ];
-            if (isset($_REQUEST['enviar'])){
-                
-            }
-            else {
-                $entradaOK = false;
-            }
-            
-            if ($entradaOK){
-                $aRespuestas;
-            }
-            else{
-            ?>
-            <form name="ejercicio23" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" novalidate>
-                    <label for="nombre">Nombre:</label>
-                    <input type="text" id="nombre" name="nombre">
-                    <br>                                       
-                    <label for="edad">Edad:</label>
-                    <input type="number" id="edad" name="edad">
-                    <br>                   
-                    <input type="submit" name="enviar" value="enviar">
+                /**
+                 * @author Víctor García Gordón
+                 * @version Fecha de última modificación 14/10/2024
+                 */
+
+                //Incluimos la libreria de validacion de formularios
+                require_once('../core/231018libreriaValidacion.php');
+
+                //Inicializacion de las variables
+                $entradaOK = true; //Variable que nos indica que todo va bienç
+                $aErrores = []; //Array donde recogemos los mensajes de error
+                $aRespuestas = []; //Array donde recogeremos la respuestas correctas (si $entradaOK)
+
+
+                // Verifica si el formulario ha sido enviado
+                if (isset($_REQUEST['enviar'])) {
+                        //Para cada campo del formulario: Validar entrada y actuar en consecuencia
+                        $aErrores = [
+                                'nombre' => validacionFormularios::comprobarAlfabetico($_REQUEST['nombre'], 50, 3, 1),
+                                'edad' => validacionFormularios::comprobarAlfaNumerico($_REQUEST['edad'], 2, 1, 1),
+                        ];
+
+                        //Recorremos el array de errores
+                        foreach ($aErrores as $clave => $valor) {
+                                if ($valor == !null) {
+                                        $entradaOK = false;
+                                }
+                        }
+                } else {
+                        //El formulario no se ha rellenado nunca
+                        $entradaOK = false;
+                }
+
+                //Tratamiento del formulario
+                if ($entradaOK) {
+                        //Almacenamos el valor en el array
+                        $aRespuestas = [
+                                'nombre' => $_REQUEST['nombre'],
+                                'edad' => $_REQUEST['edad'],
+                        ];
+
+                        //Mostramos las respuestas por pantalla
+                        echo "<h1>Respuestas:</h1>";
+                        foreach ($aRespuestas as $key => $value) {
+                                echo "$key : $value <br>";
+                        }
+                } else {
+                        //Mostrar el formulario hasta que lo rellenemos correctamente
+                ?>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                        <label for="nombre">Nombre:</label><br>
+                        <input type="text" id="nombre" name="nombre"><br>
+                        <?php if (!empty($aErrores["nombre"])) { ?>
+                                <span><?php echo $aErrores["nombre"]; ?></span>
+                        <?php } ?>
+                        <br>
+                        <label for="edad">Edad:</label><br>
+                        <input type="text" id="edad" name="edad"><br>
+                        <?php if (!empty($aErrores["edad"])) { ?>
+                                <span><?php echo $aErrores["edad"]; ?></span>
+                        <?php } ?>
+                        <br>
+                        <input name="enviar" type="submit" value="Enviar">
                 </form>
-            <?php    
-            }          
-            ?>
+                <?php
+                }
+                ?>
         </main>
         <footer>
             <div>
