f631c2a4fce4b4460995e7ff820f664984978344
victorgargor <victor.gargor@educa.jcyl.es>
2024-10-17 13:03:35 +0200

    Clase 17/10/2024

--- codigoPHP/ejercicio24.php
+++ codigoPHP/ejercicio24.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../webroot/css/index.css" type="text/css">
+        <link rel="stylesheet" href="../webroot/css/formularios.css" type="text/css">
         <title>Víctor García Gordón</title>
     </head>
     <body>
@@ -22,25 +22,33 @@
 
                 //Incluimos la libreria de validacion de formularios
                 require_once('../core/231018libreriaValidacion.php');
+                
+                //Definición de constantes
+                define("OBLIGATORIO",1);
+                define("OPCIONAL",0);
 
                 //Inicializacion de las variables
                 $entradaOK = true; //Variable que nos indica que todo va bien
                 $oFechaActual = new DateTime("now"); //Variable que recoge la fecha actual
                 //Array donde recogemos los mensajes de error
-                $aErrores = ['nombre' => '', 
-                    'edad' => ''
+                $aErrores = ['nombre' => '',
+                    'fechaNacimiento' => '',
+                    'edad' => '',
+                    'fechaActual' => ''
                 ]; 
                 //Array donde recogeremos la respuestas correctas (si $entradaOK)
-                $aRespuestas = ['nombre' => '', 
-                    'edad' => ''
+                $aRespuestas = ['nombre' => '',
+                    'fechaNacimiento' => '',
+                    'edad' => '',
+                    'fechaActual' => ''
                 ];
-
 
                 // Verifica si el formulario ha sido enviado
                 if (isset($_REQUEST['enviar'])) {
                         //Para cada campo del formulario: Validar entrada y actuar en consecuencia
-                        $aErrores['nombre'] = validacionFormularios::comprobarAlfabetico($_REQUEST['nombre'], 1000, 1, 1);
-                        $aErrores['edad'] = validacionFormularios::comprobarEntero($_REQUEST['edad'], 200, 1, 1);
+                        $aErrores['nombre'] = validacionFormularios::comprobarAlfabetico($_REQUEST['nombre'], 1000, 1, OBLIGATORIO);
+                        $aErrores['edad'] = validacionFormularios::comprobarEntero($_REQUEST['edad'], 200, 1, OPCIONAL);
+                        $aErrores['fechaNacimiento'] = validacionFormularios::validarFecha($_REQUEST['fechaNacimiento'], date_format($oFechaActual, 'd-m-Y'), '01/01/1900', OBLIGATORIO);
 
                         //Recorremos el array de errores
                         foreach ($aErrores as $clave => $valor) {
@@ -60,14 +68,18 @@
                         //Almacenamos el valor en el array
                         $aRespuestas = [
                                 'nombre' => $_REQUEST['nombre'],
+                                'fechaNacimiento' => $_REQUEST['fechaNacimiento'],
                                 'edad' => $_REQUEST['edad'],
+                                'fechaActual' => date_format($oFechaActual, 'd-m-Y')
                         ];
 
                         //Mostramos las respuestas por pantalla
+                        echo '<div class="responses">';
                         echo "<h2>Respuestas:</h2>";
                         foreach ($aRespuestas as $key => $value) {
                                 echo "$key : $value <br>";
                         }
+                        echo '</div>'; 
                 } else {
                         //Mostrar el formulario hasta que lo rellenemos correctamente
                 ?>
@@ -79,15 +91,22 @@
                                 <span style="color: red"><?php echo $aErrores["nombre"]; ?></span>
                         <?php } ?>                
                         <br>
+                        <label for="fechaNacimiento">Fecha de nacimiento: 
+                        <input type="date" id="fechaNacimiento" name="fechaNacimiento" required style="background-color: yellow" value="<?php echo (isset($_REQUEST['fechaNacimiento'])?$_REQUEST['fechaNacimiento']:''); ?>">
+                        </label>
+                        <?php if (!empty($aErrores["fechaNacimiento"])) { ?>
+                                <span style="color: red"><?php echo $aErrores["fechaNacimiento"]; ?></span>
+                        <?php } ?>                
+                        <br>  
                         <label for="edad">Edad:
-                            <input type="edad" id="edad" name="edad" style="background-color: white" value="<?php echo (isset($_REQUEST['edad'])?$_REQUEST['edad']:''); ?>">
+                        <input type="text" id="edad" name="edad" style="background-color: white" value="<?php echo (isset($_REQUEST['edad'])?$_REQUEST['edad']:''); ?>">
                         </label>
                         <?php if (!empty($aErrores["edad"])) { ?>
                         <span style="color: red"><?php echo $aErrores["edad"]; ?></span>
                         <?php } ?>             
                         <br>
-                        <label for="fecha">Fecha Actual:
-                            <input type="text" id="fecha" name="fecha" value="<?php echo date_format($oFechaActual, 'd-m-Y')?>" style="background-color: lightgray" disabled>
+                        <label for="fechaActual">Fecha Actual:
+                        <input type="text" id="fechaActual" name="fechaActual" value="<?php echo date_format($oFechaActual, 'd-m-Y')?>" style="background-color: lightgray" disabled>
                         </label>
                         <br>
                         <input name="enviar" type="submit" value="Enviar">
