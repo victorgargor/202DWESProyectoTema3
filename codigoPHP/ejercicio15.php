2e5699ff17e925d05d1a1b45f781eecb638d14ae
victorgargor <victor.gargor@educa.jcyl.es>
2024-11-21 18:47:58 +0100

    Revert "Actualizacion css 15/11/2024"
    This reverts commit 432ad54fcbbcb7124f30e672a449f00ef2cf5092.

--- codigoPHP/ejercicio15.php
+++ codigoPHP/ejercicio15.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../webroot/css/index.css" type="text/css">
+        <link rel="stylesheet" href="../webroot/css/ejercicios.css" type="text/css">
         <title>Víctor García Gordón</title>
     </head>
     <body>
